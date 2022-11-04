41ef15d4d9207210af61c5b0458561ca730014be
ynnk <david.f.mendes@proton.me>
2022-11-04 11:57:39 +0000

    Added first iteration of the character's stats

---
+++ TextCombat.py
@@ -0,0 +1,6 @@
+warrior_stats = [32, 5, 2, 5, 2] #Warrior stats in order: HP, MP, AP, WP, Init
+
+priest_stats = [20, 25, 0, 2, 6] #Priest stats in order: HP, MP, AP, WP, Init
+
+orc_stats = [15, 0, 2, 2, 2] #Orc stats in order: HP, MP, AP, WP, Init
+
