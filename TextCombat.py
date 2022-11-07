b8da15906edacd67159f00e643e85e278ea0cb1d
ynnk <david.f.mendes@proton.me>
2022-11-07 21:29:57 +0000

    Changed character's stats to classes

--- TextCombat.py
+++ TextCombat.py
@@ -1,6 +1,13 @@
-warrior_stats = [32, 5, 2, 5, 2] #Warrior stats in order: HP, MP, AP, WP, Init
+class char_stats:
+    def __init__(stat, hp, mp, ap, wp, init):
+        stat.hp = hp 
+        stat.mp = mp 
+        stat.ap = ap
+        stat.wp = wp
+        stat.init = init
 
-priest_stats = [20, 25, 0, 2, 6] #Priest stats in order: HP, MP, AP, WP, Init
+warrior = char_stats(32, 5, 2, 5, 2) #Warrior stats in order: HP, MP, AP, WP, Init
 
-orc_stats = [15, 0, 2, 2, 2] #Orc stats in order: HP, MP, AP, WP, Init
+priest = char_stats(20, 25, 0, 2,6) #Priest stats in order: HP, MP, AP, WP, Init
 
+orc = char_stats(15, 0, 2, 2,2) #Orc stats in order: HP, MP, AP, WP, Init
