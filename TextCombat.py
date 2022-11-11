131bd998ca9c8191fb904f9c09059e1febca2bff
ynnk <david.f.mendes@proton.me>
2022-11-11 10:42:51 +0000

    Added first version of the actionphase and character spell funcions:

--- TextCombat.py
+++ TextCombat.py
@@ -1,7 +1,8 @@
-import random
+import random, os
 
 class char_stats:
-    def __init__(stat, hp, mp, ap, wp, init, d20):
+    def __init__(stat, name, hp, mp, ap, wp, init, d20):
+        stat.name = name
         stat.hp = hp 
         stat.mp = mp 
         stat.ap = ap
@@ -9,17 +10,17 @@ class char_stats:
         stat.init = init
         stat.roll = d20
 
-warrior = char_stats(32, 5, 2, 5, 2, 0) #Warrior stats in order: HP, MP, AP, WP, Init, dice roll
+warrior = char_stats("warrior", 32, 5, 2, 5, 2, 0) #Warrior stats in order: Name, HP, MP, AP, WP, Init, dice roll
 
-priest = char_stats(20, 25, 0, 2, 6, 0) #Priest stats in order: HP, MP, AP, WP, Init, dice roll
+priest = char_stats("priest", 20, 25, 0, 2, 6, 0) #Priest stats in order: Name, HP, MP, AP, WP, Init, dice roll
 
-orc = char_stats(15, 0, 2, 2, 2, 0) #Orc stats in order: HP, MP, AP, WP, Init, dice roll
+orc = char_stats("orc", 15, 0, 2, 2, 2, 0) #Orc stats in order: Name, HP, MP, AP, WP, Init, dice roll
 
-goblin = char_stats(14, 0, 2, 2, 2, 0) #Goblin stats in order: HP, MP, AP, WP, Init, dice roll
+goblin = char_stats("goblin", 14, 0, 2, 2, 2, 0) #Goblin stats in order: Name, HP, MP, AP, WP, Init, dice roll
 
-hobgoblin = char_stats(13, 0, 2, 2, 2, 0) #Hobgoblin stats in order: HP, MP, AP, WP, Init, dice roll
+hobgoblin = char_stats("hobgoblin", 13, 0, 2, 2, 2, 0) #Hobgoblin stats in order: Name, HP, MP, AP, WP, Init, dice roll
 
-kobold = char_stats(12, 0, 2, 2, 2, 0) #Kobold stats in order: HP, MP, AP, WP, Init, dice roll
+kobold = char_stats("kobold", 12, 0, 2, 2, 2, 0) #Kobold stats in order: Name, HP, MP, AP, WP, Init, dice roll
 
 enemy = [orc, goblin, hobgoblin, kobold] #list of enemy chars
 
@@ -29,13 +30,16 @@ atk_words = ["atk","Atk", "attack", "Attack"]
 
 magic_words = ["mag", "Mag", "magic", "Magic"]
 
-rushdown = ["Rushes into the enemy, tackling them into the ground. Inflicts between 6 and 11 damage. Costs 5 mana."]
+rushdown = ["Rushes into the enemy, tackling them into the ground. Inflicts between 6 and 9 damage. Costs 5 mana."]
 
-exorcism = ["Corrupt and enemy's life essence. Inflicts between 2 and 12 damage. Costs 5 mana."]
+exorcism = ["Corrupt and enemy's life essence. Inflicts between 2 and 8 damage. Costs 5 mana."]
 
 mend = ["Repay some of a character's current debt to the Death god. Heals a character between 3 and 8. Costs 3 mana."]
 
 def ui(): #Basic ui (Might change later)
+    clearconsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+    clearconsole()
+
     print("Enemy: Orc            Enemy: Goblin           Enemy: Hobgoblin           Enemy: Kobold")
     print(f"HP:{orc.hp}                 HP:{goblin.hp}                   HP:{hobgoblin.hp}                      HP:{kobold.hp}")
     print(f"MP:{orc.mp}                  MP:{goblin.mp}                    MP:{hobgoblin.mp}                       MP:{kobold.mp}")
@@ -53,7 +57,7 @@ def ui(): #Basic ui (Might change later)
 
 def alive(): #Checks if character is alive
     global alive
-    chars = [warrior, priest, orc, gob, hob, kob]
+    chars = [warrior, priest, orc, goblin, hobgoblin, kobold]
     alive = []
     for char in chars:
         if char.hp > 0:
@@ -65,10 +69,12 @@ def initiative(): #Rolls a dice for each character and sorts them in order of mo
     def d20(): # Code based on answer by user jws1 on StackOverflow. (https://stackoverflow.com/questions/60193710/python-d20-dice-rolling-program)
         total = 0
         total += random.randint(1,20)
+        return total
 
     for char in alive:
         char.d20 = d20() + char.init
-        atkorder = sorted(alive, key=lambda h: (h.d20)) # Code based on answer by user falsetru on StackOverflow (https://stackoverflow.com/questions/26310394/python-sort-a-list-of-objects-based-on-their-attributes)
+        atkorder = sorted(alive, key=lambda x: (x.roll)) # Code based on answer by user falsetru on StackOverflow (https://stackoverflow.com/questions/26310394/python-sort-a-list-of-objects-based-on-their-attributes)
+        print(atkorder)
 
 def Round(): #Gets a character from the initiative order and calculate's its actions.
     def dmg_calc(attacking, defending):
@@ -76,34 +82,83 @@ def Round(): #Gets a character from the initiative order and calculate's its act
         if dmg_fin < 0:
             dmg_fin = 0
 
+    def d4():
+        total = 0
+        total += random.randint(1,4)
+
+    def d6():
+        total = 0
+        total += random.randint(1,6)
+
+    def rushdown(attacking, defending):
+        dmg = attacking.wp + d4
+        dmg_fin = defending.ap - dmg
+        if dmg_fin < 0:
+            dmg_fin = 0
+    
+    def exorcism(attacking, defending):
+        dmg = d4 * 2
+        dmg_fin = defending.ap - dmg
+        if dmg_fin < 0:
+            dmg_fin = 0
+
+    def mend(healer, healed):
+        heal = healer.wp + d6
+        healed.hp = healed.hp + heal
+
+
     for x in range(1,6): #Selects each character in order of initiative
 
         if atkorder[x] in enemy: #if selected character is an enemy just calculates its dmg
             chosen = random.choice[warrior, priest] #chooses randomly between the 2 allied characters to attack
             chosen.hp = chosen.hp - dmg_calc(atkorder[x])
-            print(f"\nEnemy {atkorder[x]} attacked allied {chosen} and dealt {dmg_calc} damage.")
+            current_char = atkorder[x].name
+            print(f"\nEnemy {current_char} attacked allied {chosen} and dealt {dmg_calc} damage.")
+            ui()
             
         elif atkorder[x] in ally: #if selected character is an ally it enters the action screen
-            print(f"It's time for {atkorder[x]} to do an action!\nWhat should {atkorder[x]} do?\nThe {atkorder[x]} can either Attack or cast Magic")
+            print(f"It's time for {current_char} to do an action!\nWhat should {current_char} do?\nThe {current_char} can either Attack or cast Magic")
             action = input()
 
             if action in atk_words: #Attack action was chosen
                 while check == 1:
-                    print(f"Which enemy should {atkorder[x]} attack?")
+                    print(f"Which enemy should {current_char} attack?")
                     atkenemy = input()
-                    if atkenemy in enemy:
-                        atkenemy.hp = dmg_calc(atkorder[x], atkenemy)
-                        print(f"The allied {atkorder[x]} attacked enemy {atkenemy} and dealt {dmg_calc} damage.")
+                    if atkenemy in enemy: #Checks if targeted character is an enemy
+                        atkenemy.hp = dmg_calc(current_char, atkenemy)
+                        print(f"The allied {current_char} attacked enemy {atkenemy} and dealt {dmg_calc} damage.")
                         check = 0
                     else:
-                        print(f"\nChosen target not valid\n")
+                        print(f"\nChosen target not valid.\n")
 
             elif action in magic_words: #Magic action was chosen
-                print(f"The {atkorder[x]} can cast the following magic:")
+                print(f"The {current_char} can cast the following magic:")
                 while check == 1:
-                    if atkorder[x] == warrior:
+                    if current_char == warrior:  #checks if current character is a warrior
                         print(rushdown)
-                    elif atkorder[x] == priest:
+                        target = input(f"Who do you want to attack?\n")
+                        if target in enemy: #checksif target is an enemy
+                            rushdown(current_char, target)
+                            check = 0
+                        else:
+                            print("Invalid target")
+                    elif current_char == priest: #checks if current character is a priest
+                        print(f"{exorcism}\n{mend}")
+                        spell = input(f"Which spell do you want to use?\n")
+                        if spell == "Exorcism" or spell == "exorcism":
+                            target = f"Who do you want to attack?\n"
+                            if target in enemy: #checks if target is an enemy
+                                exorcism(current_char, target)
+                                check = 0
+                            else: 
+                                print("Chosen target not valid.")
+                        if spell == "Mend" or spell == "mend":
+                            target = f"Who do you want to heal?\n"
+                            if target in ally:  #checks if target is an ally
+                                mend(current_char, target)
+                                check = 0
+                            else:
+                                print("Chosen target not valid")
             else:
                 print("The character cant do that action.")
 
