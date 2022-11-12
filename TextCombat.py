9dc4823d993c6fcc3229289ffaccf2970d960706
ynnk <david.f.mendes@proton.me>
2022-11-12 17:12:36 +0000

    Finished main loop v1 and started changing ui for a more usable version.

--- TextCombat.py
+++ TextCombat.py
@@ -1,26 +1,27 @@
-import random, os
+import random, os, keyboard
 
 class char_stats:
-    def __init__(stat, name, hp, mp, ap, wp, init, d20):
+    def __init__(stat, name, max_hp, hp, mp, ap, wp, init, d20):
         stat.name = name
         stat.hp = hp 
+        stat.max_hp = max_hp
         stat.mp = mp 
         stat.ap = ap
         stat.wp = wp
         stat.init = init
         stat.d20 = d20
 
-warrior = char_stats("warrior", 32, 5, 2, 5, 2, 0) #Warrior stats in order: Name, HP, MP, AP, WP, Init, dice roll
+warrior = char_stats("warrior",32 , 32, 5, 2, 5, 2, 0) #Warrior stats in order: Name,MaxHP , current HP, MP, AP, WP, Init, dice roll
 
-priest = char_stats("priest", 20, 25, 0, 2, 6, 0) #Priest stats in order: Name, HP, MP, AP, WP, Init, dice roll
+priest = char_stats("priest", 20, 20, 25, 0, 2, 6, 0) #Priest stats in order: Name, MaxHp, current HP, MP, AP, WP, Init, dice roll
 
-orc = char_stats("orc", 15, 0, 2, 2, 2, 0) #Orc stats in order: Name, HP, MP, AP, WP, Init, dice roll
+orc = char_stats("orc", 15, 15, 0, 2, 2, 2, 0) #Orc stats in order: Name, MaxHP, current HP, MP, AP, WP, Init, dice roll
 
-goblin = char_stats("goblin", 14, 0, 2, 2, 2, 0) #Goblin stats in order: Name, HP, MP, AP, WP, Init, dice roll
+goblin = char_stats("goblin", 14, 14, 0, 2, 2, 2, 0) #Goblin stats in order: Name, MaxHP, current HP, MP, AP, WP, Init, dice roll
 
-hobgoblin = char_stats("hobgoblin", 13, 0, 2, 2, 2, 0) #Hobgoblin stats in order: Name, HP, MP, AP, WP, Init, dice roll
+hobgoblin = char_stats("hobgoblin",13, 13, 0, 2, 2, 2, 0) #Hobgoblin stats in order: Name, MaxHP, current HP, MP, AP, WP, Init, dice roll
 
-kobold = char_stats("kobold", 12, 0, 2, 2, 2, 0) #Kobold stats in order: Name, HP, MP, AP, WP, Init, dice roll
+kobold = char_stats("kobold", 12, 12, 0, 2, 2, 2, 0) #Kobold stats in order: Name, MaxHP, current HP MP, AP, WP, Init, dice roll
 
 enemy = ("orc","goblin", "hobgoblin", "kobold") #Tuple of enemy chars
 
@@ -30,33 +31,29 @@ atk_words = ["atk","Atk", "attack", "Attack"]
 
 magic_words = ["mag", "Mag", "magic", "Magic"]
 
-rush_desc = ["Rushdown: Rushes into the enemy, tackling them into the ground. Inflicts between 6 and 9 damage. Costs 5 mana."]
+rush_desc = "Rushdown: Rushes into the enemy, tackling them into the ground. Inflicts between 6 and 9 damage. Costs 5 mana.\n"
 
-exor_desc = ["Exorcism: Corrupt and enemy's life essence. Inflicts between 2 and 8 damage. Costs 5 mana."]
+exor_desc = "Exorcism: Corrupt and enemy's life essence. Inflicts between 2 and 8 damage. Costs 5 mana.\n"
 
-mend_desc = ["Mend: Repay some of a character's current debt to the Death god. Heals a character between 3 and 8. Costs 3 mana."]
+mend_desc = "Mend: Repay some of a character's current debt to the Death god. Heals a character between 3 and 8. Costs 3 mana.\n"
 
 def ui(): #Basic ui (Might change later)
     clearconsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-    clearconsole()
+    clearconsole() #clears the console so it doesn't get cluttered
 
-    print("Enemy: Orc            Enemy: Goblin           Enemy: Hobgoblin           Enemy: Kobold")
-    print(f"HP:{orc.hp}                 HP:{goblin.hp}                   HP:{hobgoblin.hp}                      HP:{kobold.hp}")
-    print(f"MP:{orc.mp}                  MP:{goblin.mp}                    MP:{hobgoblin.mp}                       MP:{kobold.mp}")
-    print(f"AP:{orc.ap}                  AP:{goblin.ap}                    AP:{hobgoblin.ap}                       AP:{kobold.ap}")
-    print(f"WP:{orc.wp}                  WP:{goblin.wp}                    WP:{hobgoblin.wp}                       WP:{kobold.wp}")
-    print(f"Init:{orc.init}                Init:{goblin.init}                  Init:{hobgoblin.init}                     Init:{kobold.init}")
-
-    print(f"\n\n\n\n\n")
-    print(f"Ally: Warrior               Ally: Priest")
-    print(f"HP:{warrior.hp}                       HP:{priest.hp}")
-    print(f"MP:{warrior.mp}                        MP:{priest.mp}")
-    print(f"AP:{warrior.ap}                        AP:{priest.ap}")
-    print(f"WP:{warrior.wp}                        WP:{priest.wp}")
-    print(f"Init:{warrior.init}                      InitP:{priest.init}")
+    alive()
+    ui_enemy = [] # List of all enemy characters alive to print in ui
+    ui_ally = [] # List of all allied characters alive to print in ui
+    for character in chars:
+        if character in alive and character in enemy: #checks if a character is alive and is an enemy
+            ui_enemy.append(character)
+        elif character in alive and character in ally: #checks if a character is alive and is an ally
+            ui_ally.append(character)
+    for character in ui_enemy:
+        print(f"Enemy:{ui.enemy[character]} Hp:{ui.enemy[character].hp} Armor")
 
 def alive(): #Checks if character is alive
-    global alive
+    global alive, chars
     chars = [warrior, priest, orc, goblin, hobgoblin, kobold]
     alive = []
     for char in chars:
@@ -77,6 +74,13 @@ def initiative(): #Rolls a dice for each character and sorts them in order of mo
         atkorder.sort(key=lambda x: (x.d20), reverse = True) # Code based on answer by user Art on StackOverflow (https://stackoverflow.com/questions/67750705/python-sorting-based-on-class-attribute)
 
 def Round(): #Gets a character from the initiative order and calculates its actions.
+    def end_action(): #This makes so the code only continues when the down arrow is pressed
+        print("press down arrow to proceed")
+        while True:
+            if keyboard.is_pressed('down'):
+                break
+        ui()
+
     def dmg_calc(attacking, defending):
         dmg_fin = attacking.wp - defending.ap
         if dmg_fin < 0:
@@ -109,19 +113,21 @@ def Round(): #Gets a character from the initiative order and calculates its acti
 
     def rushdown(attacking, defending):
         dmg = attacking.wp + d4()
-        dmg_fin = defending.ap - dmg
-        if dmg_fin < 0:
-            dmg_fin = 0
+        defending.hp = defending.hp - dmg
+        print(f"The {attacking.name} rushed into the {defending.name} and delt {dmg} damage")
     
-    def exorcism(defending):
+    def exorcism(attacking, defending):
         dmg = d4() * 2
-        dmg_fin = defending.ap - dmg
-        if dmg_fin < 0:
-            dmg_fin = 0
+        defending.hp = defending.hp - dmg
+        print(f"The {attacking.name} exorcised the {defending.name} and delt {dmg} damage")
 
     def mend(healer, healed):
         heal = healer.wp + d6()
-        healed.hp = healed.hp + heal
+        if healed.max_hp < healed.hp + heal:
+            healed.hp = healed.max_hp
+        else:
+            healed.hp = healed.hp + heal
+        print(f"The {healer.name} healed the {healed.name} for {heal} damage")
 
     num_char = -1 #This refers to the caracter in the list of characters in order of initiative and goes up by one for each character
 
@@ -130,9 +136,10 @@ def Round(): #Gets a character from the initiative order and calculates its acti
         name_char = atkorder[num_char].name #gets the character name so the rest of the code can identify which character it is refering to
 
         if name_char in enemy: #if selected character is an enemy just calculates its dmg
-            chosen = random.choice((warrior, priest)) #chooses randomly between the 2 allied characters to attack
+            chosen = random.choice((ally)) #chooses randomly one allied characters to attack
             chosen.hp = chosen.hp - dmg_calc(char, chosen)
             print(f"\nEnemy {name_char} attacked allied {chosen.name} and dealt {dmg_calc(char, chosen)} damage.")
+            end_action()
             
         elif name_char in ally: #if selected character is an ally it enters the action screen
             check = 1 # this check exists so the code can know when an allied character has finished its turn
@@ -145,12 +152,13 @@ def Round(): #Gets a character from the initiative order and calculates its acti
                             atkenemy = answer_into_character(atkenemy)
                             atkenemy.hp = dmg_calc(char, atkenemy)
                             print(f"The allied {name_char} attacked enemy {atkenemy.name} and dealt {dmg_calc(char, atkenemy)} damage.")
+                            end_action() #refreshes ui and stops code until user wishes to proceed
                             check = 0
                         else:
                             print(f"\nChosen target not valid.\n")
 
                 elif action in magic_words: #Magic action was chosen
-                    print(f"The alied {name_char} can cast the following magic:")
+                    print(f"The alied {name_char} can cast the following magic:\n")
                     while check == 1:
 
                         if char == warrior:  #checks if current character is a warrior
@@ -159,9 +167,10 @@ def Round(): #Gets a character from the initiative order and calculates its acti
                             if target in enemy: #checks if target is an enemy
                                 target = answer_into_character(target)
                                 rushdown(char, target)
+                                end_action() #refreshes ui and stops code until user wishes to proceed
                                 check = 0
                             else:
-                                print("Invalid target")
+                                print("Invalid target\n")
 
                         elif char == priest: #checks if current character is a priest
                             print(f"{exor_desc}\n{mend_desc}")
@@ -170,18 +179,20 @@ def Round(): #Gets a character from the initiative order and calculates its acti
                                 target = input(f"Who do you want to attack?\n")
                                 if target in enemy: #checks if target is an enemy
                                     target = answer_into_character(target)
-                                    exorcism(target)
+                                    exorcism(char, target)
+                                    end_action() #refreshes ui and stops code until user wishes to proceed
                                     check = 0
                                 else: 
-                                    print("Chosen target not valid.")
+                                    print("Chosen target not valid.\n")
                             elif spell == "Mend" or spell == "mend":
                                 target = input(f"Who do you want to heal?\n")
                                 if target in ally:  #checks if target is an ally
                                     target = answer_into_character(target)
                                     mend(char, target)
+                                    end_action() #refreshes ui and stops code until user wishes to proceed
                                     check = 0
                                 else:
-                                    print("Chosen target not valid")
+                                    print("Chosen target not valid\n")
                 else:
                     print("The character cant do that action.")
 
@@ -191,3 +202,24 @@ ui()
 alive()
 initiative()
 Round()
+
+
+
+def ui(): #Basic ui (Might change later)
+    clearconsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+    clearconsole()
+
+    print("Enemy: Orc            Enemy: Goblin           Enemy: Hobgoblin           Enemy: Kobold")
+    print(f"HP:{orc.hp}                 HP:{goblin.hp}                   HP:{hobgoblin.hp}                      HP:{kobold.hp}")
+    print(f"MP:{orc.mp}                  MP:{goblin.mp}                    MP:{hobgoblin.mp}                       MP:{kobold.mp}")
+    print(f"AP:{orc.ap}                  AP:{goblin.ap}                    AP:{hobgoblin.ap}                       AP:{kobold.ap}")
+    print(f"WP:{orc.wp}                  WP:{goblin.wp}                    WP:{hobgoblin.wp}                       WP:{kobold.wp}")
+    print(f"Init:{orc.init}                Init:{goblin.init}                  Init:{hobgoblin.init}                     Init:{kobold.init}")
+
+    print(f"\n\n\n\n\n")
+    print(f"Ally: Warrior               Ally: Priest")
+    print(f"HP:{warrior.hp}                       HP:{priest.hp}")
+    print(f"MP:{warrior.mp}                        MP:{priest.mp}")
+    print(f"AP:{warrior.ap}                        AP:{priest.ap}")
+    print(f"WP:{warrior.wp}                        WP:{priest.wp}")
+    print(f"Init:{warrior.init}                      InitP:{priest.init}")
