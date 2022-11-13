6ca18f73c26469fb866faa5c5b651bf2b9bab866
Guilherme Negrinho <115217696+cupstealer@users.noreply.github.com>
2022-11-13 16:58:10 +0000

    Add files via upload
    NOW IT'S WORKING! :D May update later, if I'm feeling fancy

--- Inventory.py
+++ Inventory.py
@@ -1,20 +1,21 @@
 
-# Inventory Implementation. Be add after the final version.
-
-inventory = ["Health Potion", "Mana Potion", "Revival Item"]
+# Inventory Implementation. To be add after the final version.
 
 print ("Oh right! Before you start fighitng, you can also use some items in your inventory to help your character's status in combat!（ミ￣ー￣ミ）")
-print ("I will quickly show you your available items then.")
+print ("I will quickly show you your available items.")
+print()
 
-def freq(str):
-    print()
-    str_list = str.split()
-    unique_words = set(str_list)
-    for words in unique_words :
-        print( str_list.count(words))
+def freq(str): #Based on Code Python3 (https://www.geeksforgeeks.org/find-frequency-of-each-word-in-a-string-in-python/)
+    unique_words = set(str)
+
+    for words in unique_words:
+        print('You have', str.count(words), words)
 if __name__ == "__main__":
-     
-    str = ["Health Potion", "Mana Potion", "Revival Item"]
+    str = "Bonus Damage Item (+2)", "Mana Potion (+10)", "Revival Item", "Whistle"
+ 
     freq(str)
+    print()
+
+    print("That's all, not much but we are short on budget. However, you can find out what the whistle does later on.")
 
 
